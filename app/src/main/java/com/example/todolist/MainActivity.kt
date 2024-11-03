58f6f3fa37560700744fd72a604184f254f2f03a
ivyana1992 <anayviurbaneja@gmail.com>
2024-11-03 15:27:24 -0300

    commit inicial com as modificacoes realizadas pelo Elias e a Anayvi

--- app/src/main/java/com/example/todolist/MainActivity.kt
+++ app/src/main/java/com/example/todolist/MainActivity.kt
@@ -5,16 +5,29 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.widget.ImageView;
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+
+            val ButtonEditar = findViewById(R.id.btnEditar)
+            ButtonEditar.setImageResource(R.drawable.baseline_edit_24)
+
+            val buttonExcluir = findViewById(R.id.btnExcluir)
+            buttonExcluir.setImageResource(R.drawable.baseline_delete_forever_24)
+
+            val textView = findViewById(R.id.tvTarefa1)
+            textView.text = "Nova Tarefa"
+
         }
     }
+
 }
